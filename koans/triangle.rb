52d7d9248b3d29e28152e0634a0a853211385620
Shanmukha Sahiti Challa <shanmukhasahiti@ucsb.edu>
2024-10-13 13:34:38 -0700

    65%

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,18 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  x,y,z=[a,b,c].sort
+  raise TriangleError.new("Side length cannot be zero or negative") if x<=0
+  raise TriangleError.new("Sum of two sides should be greater than the third size") if x+y<=z
+
+  if a == b && a == c
+    return :equilateral
+  end
+  if a == b || a == c || b == c
+    return :isosceles
+  else
+    return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
